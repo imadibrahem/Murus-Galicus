81474815b3f73b13eff292a9b0816e95a2fb366d
Imad Ibrahem <imadibrahem23@gmail.com>
2025-02-24 17:05:22 +0100

    Evaluation Function seventh Habitat added

---
+++ src/model/evolutionTheory/habitats/SeventhHabitatEvaluationFunction.java
@@ -0,0 +1,10 @@
+package model.evolutionTheory.habitats;
+
+import model.Board;
+import model.evaluationFunction.EvaluationFunction;
+
+public class SeventhHabitatEvaluationFunction extends EvaluationFunction {
+    public SeventhHabitatEvaluationFunction (Board board){
+        super(board, new int[]{2, 0, 2, 1, 2, 2, 2}, new int[]{1, 5, 1, 2}, new int[]{6, 14, 5, 4, 5, 2, 5}, new int[]{1, 0, 3, 5}, new int[]{9, 3, 1, 3, -5, 1, 11, 6}, new int[]{-118, -204, 0, -34, 0, 34, 204, 118}, 26, 32, 2);
+    }
+}
