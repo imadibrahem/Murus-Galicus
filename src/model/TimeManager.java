827e3e258b0e4010b267ab797d34096589c32a6e
Your Name <you@example.com>
2025-03-05 15:21:14 +0100

    TimeManger updated

--- src/model/TimeManager.java
+++ src/model/TimeManager.java
@@ -20,11 +20,13 @@ public class TimeManager {
         this.midFactor = midFactor;
         this.endFactor = endFactor;
         this.moveTimes = new double[midGameMoves + earlyGameMoves];
+        this.remainingTime = totalTime;
         distributeTime();
     }
 
     private void distributeTime() {
         double earlyTime = totalTime * earlyFactor;
+        System.out.println("Early time: " + earlyTime);
         double[] tempEarlyTimes = new double[earlyGameMoves];
         double sumEarly = 0;
         for (int i = 0; i < earlyGameMoves; i++) {
@@ -39,29 +41,48 @@ public class TimeManager {
         double scaleFactor = earlyTime / sumEarly;
         for (int i = 0; i < earlyGameMoves; i++) {
             moveTimes[i] = tempEarlyTimes[i] * scaleFactor;
+            System.out.println("Early Move:" + i + " Time: " + moveTimes[i]);
         }
-        // Allocate 20% for moves 18-30 (equal distribution)
+        // Allocate midFactor for midGameMoves  (equal distribution)
+        System.out.println("+++++++++++++++++++++++++++++++++");
         double midGameTime = totalTime * midFactor;
+        System.out.println("Mid time: " + midGameTime);
         double midMoveTime = midGameTime / midGameMoves;
-        for (int i = earlyGameMoves; i < midGameMoves; i++) {
+        System.out.println("midMoveTime: " + midMoveTime);
+        System.out.println("+++++++++++++++++++++++++++++++++");
+        for (int i = earlyGameMoves; i < (earlyGameMoves + midGameMoves); i++) {
             moveTimes[i] = midMoveTime;
+            System.out.println("Mid Move:" + i + " Time: " + moveTimes[i]);
         }
+        System.out.println("+++++++++++++++++++++++++++++++++");
+        System.out.println("+++++++++++++++++++++++++++++++++");
+
     }
 
     public double getTimeForMove(int moveNumber) {
         double usedTime;
         if (moveNumber < moveTimes.length && moveTimes[moveNumber] > 0) {
             usedTime = Math.min(moveTimes[moveNumber], remainingTime);
-            updateRemainingTime(usedTime + 0.05);
+            updateRemainingTime(usedTime);//todo remove and replace with time from player
             return usedTime;
         }
         usedTime = remainingTime * endFactor; // Panic mode: endFactor of remaining time
-        updateRemainingTime(usedTime + 0.05);
+        updateRemainingTime(usedTime);//todo remove and replace with time from player
         return usedTime;
     }
 
     public void updateRemainingTime(double usedTime) {
         remainingTime -= usedTime;
         if (remainingTime < 0) remainingTime = 0;
+    }
+
+    public static void main (String[] args){
+        TimeManager timeManager = new TimeManager(6000, 7, 14, 0.75, 0.2, 0.15);
+        int i = 0;
+        System.out.println("++++++++++++++++++++++++++++");
+        while (timeManager.remainingTime > 0.001){
+            System.out.println("Move:" + i +" Time:" + timeManager.getTimeForMove(i) + " remainingTime:" + timeManager.remainingTime);
+            i++;
+        }
     }
 }
